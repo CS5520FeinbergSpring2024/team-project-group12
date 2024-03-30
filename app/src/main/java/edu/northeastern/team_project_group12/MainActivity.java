210d1206ab80ac9f9857e2a0f69548c2a721f9fe
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-03-30 14:22:49 -0400

    added google-services dependencies

---
+++ app/src/main/java/edu/northeastern/team_project_group12/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.northeastern.team_project_group12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // test commit
+    }
+}
