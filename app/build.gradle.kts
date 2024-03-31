aefb9cd06e728d9ef759e5b80e8588eb2c12f738
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-03-31 15:46:27 -0400

    change min SDK to 26 to run on my device

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "edu.northeastern.group12_finalproject"
-        minSdk = 27
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
