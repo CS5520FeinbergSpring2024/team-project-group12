9d063fd5acfe29e4d2a62c2416d822a201dffbe1
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-03-31 16:16:42 -0400

    set up Post Class for Post object in database

---
+++ app/src/main/java/edu/northeastern/group12_finalproject/Post.java
@@ -0,0 +1,15 @@
+package edu.northeastern.group12_finalproject;
+
+public class Post {
+
+    private String postId; // unique identifier for Post in database
+    private String username;
+    private long timestamp;
+    private String imageUrl;
+    private String description;
+    private int active_minutes;
+    private float distance;
+
+    // constructor, getters and setters
+
+}
