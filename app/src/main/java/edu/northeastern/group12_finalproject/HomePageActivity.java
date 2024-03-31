f41afff4bea7c7ea4d2289854c630388a25190d3
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-03-31 16:06:55 -0400

    handle image being uploaded sideways

--- app/src/main/java/edu/northeastern/group12_finalproject/HomePageActivity.java
+++ app/src/main/java/edu/northeastern/group12_finalproject/HomePageActivity.java
@@ -7,6 +7,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -17,6 +19,10 @@ import android.widget.ImageView;
 import android.Manifest;
 import android.widget.Toast;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -78,10 +84,54 @@ public class HomePageActivity extends AppCompatActivity {
                         int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
                         String picturePath = cursor.getString(columnIndex);
                         cursor.close();
-                        imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                        // handle image orientation (stop photo from being uploaded sideways)
+                        Bitmap bitmap = BitmapFactory.decodeFile(picturePath);
+                        try {
+                            bitmap = rotateImageIfRequired(bitmap, selectedImage);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        imageView.setImageBitmap(bitmap);
                     }
                 }
                 break;
         }
+    }
+
+    // At first, the image was uploaded sideways - needed to handle case where image's orientation was being incorrectly interpreted
+    private Bitmap rotateImageIfRequired(Bitmap bitmap, Uri selectedImage) throws IOException {
+        try {
+            InputStream input = getContentResolver().openInputStream(selectedImage);
+            ExifInterface ei;
+            if (Build.VERSION.SDK_INT > 23) {
+                ei = new ExifInterface(input);
+            } else {
+                ei = new ExifInterface(selectedImage.getPath());
+            }
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    return rotateImage(bitmap, 90);
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    return rotateImage(bitmap, 180);
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    return rotateImage(bitmap, 270);
+                default:
+                    return bitmap;
+            }
+        } catch (IOException e) {
+            // Handle IOException here
+            e.printStackTrace();
+            return bitmap; // return original bitmap if error
+        }
+
+    }
+
+    // rotate image before uploading it so that orientation is shown correctly on the screen
+    private Bitmap rotateImage(Bitmap bitmap, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
     }
 }
