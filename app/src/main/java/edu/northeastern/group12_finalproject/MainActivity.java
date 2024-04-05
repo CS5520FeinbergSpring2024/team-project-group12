8af9c9c28028c654fcea1d5f54bf30bb6a1fba40
nquinn10 <ntquinn10@gmail.com>
2024-04-05 01:10:24 -0400

    clean up

--- app/src/main/java/edu/northeastern/group12_finalproject/MainActivity.java
+++ app/src/main/java/edu/northeastern/group12_finalproject/MainActivity.java
@@ -6,8 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
