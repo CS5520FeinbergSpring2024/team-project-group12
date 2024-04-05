1d111a0aa1afa3ce04d47a646eb01ecfcae727be
nquinn10 <ntquinn10@gmail.com>
2024-04-04 23:36:09 -0400

    set up bottom nav bar

--- app/src/main/java/edu/northeastern/group12_finalproject/MainActivity.java
+++ app/src/main/java/edu/northeastern/group12_finalproject/MainActivity.java
@@ -1,11 +1,15 @@
 package edu.northeastern.group12_finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,16 +19,47 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        // test home button to test home page activity
-        Button homeBtn = findViewById(R.id.button);
-        homeBtn.setOnClickListener(new View.OnClickListener() {
+//        Button homeBtn = findViewById(R.id.bottom_nav_home);
+//        homeBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) { openHomePage(); }
+//        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) { openHomePage(); }
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.bottom_nav_home) {
+                    openHomePage();
+                    return true;
+                } else if (item.getItemId() == R.id.bottom_nav_new_post) {
+                    openNewPostPage();
+                    return true;
+                } else if (item.getItemId() == R.id.bottom_nav_profile) {
+                    openProfilePage();
+                    return true;
+                }
+                return false;
+            }
         });
     }
 
+
+
     private void openHomePage() {
-        Intent intent = new Intent(this, HomePageActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    // Method to open New Post Activity
+    private void openNewPostPage() {
+        Intent intent = new Intent(this, CreatePostActivity.class);
+        startActivity(intent);
+    }
+
+    // Method to open Profile Activity
+    private void openProfilePage() {
+        Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
     }
 }
