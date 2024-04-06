b221e0b2b89d65409635093e7d48a9c687b99834
宋灵均 <lingjun.song@yahoo.com>
2024-04-05 23:56:26 -0400

    initialized edit profile funtionality

---
+++ app/src/main/java/edu/northeastern/group12_finalproject/EditProfileActivity.java
@@ -0,0 +1,72 @@
+package edu.northeastern.group12_finalproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    EditText nameET;
+    EditText bioET;
+    Button confirmBtn;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        nameET = findViewById(R.id.edit_name);
+        bioET = findViewById(R.id.edit_bio);
+        confirmBtn = findViewById(R.id.confirm_button);
+
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retrieveFirebaseInfo();
+            }
+        });
+    }
+
+    // Retrieve user info from firebase.
+    private void retrieveFirebaseInfo() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
+
+        // Initial View set up.
+        if ((nameET.getText().toString().isEmpty()) && (bioET.getText().toString().isEmpty())) {
+            Toast.makeText(this, "Name and bio cannot be null", Toast.LENGTH_LONG);
+        }
+
+        if (!(nameET.getText().toString().isEmpty())) {
+            databaseReference.child(user.getUid()).child("username").setValue(nameET.getText().toString());
+        }
+        if (!(bioET.getText().toString().isEmpty())) {
+            databaseReference.child(user.getUid()).child("bio").setValue(bioET.getText().toString());
+        }
+
+
+
+    }
+}
--- app/src/main/java/edu/northeastern/group12_finalproject/ProfileActivity.java
+++ app/src/main/java/edu/northeastern/group12_finalproject/ProfileActivity.java
@@ -15,11 +15,30 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    TextView profileName;
+    TextView bio;
+    TextView editTv;
+
     private ProgressBar progBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +48,18 @@ public class ProfileActivity extends AppCompatActivity {
         // Progress bar
         progBar = (ProgressBar) findViewById(R.id.profileProgressBar);
         progBar.setVisibility(View.GONE);
+
+        retrieveFirebaseInfo();
+
+        // Take you to edit profile page.
+        editTv = findViewById(R.id.tvEditProfile);
+        editTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this, EditProfileActivity.class));
+                finish();
+            }
+        });
 
         // set up of bottom nav bar
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -92,5 +123,40 @@ public class ProfileActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CreatePostActivity.class);
         startActivity(intent);
         finish(); // Close current activity
+    }
+    // Retrieve user info from firebase.
+    private void retrieveFirebaseInfo() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
+
+        // Initial View set up.
+        profileName = findViewById(R.id.profileName);
+        bio = findViewById(R.id.bio);
+
+        Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // Check until required data gets updated
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                    // Retrieve data
+                    String name = "" + dataSnapshot.child("username").getValue();
+                    String bioData = "" + dataSnapshot.child("bio").getValue();
+
+                    // Set data to textView.
+                    profileName.setText(name);
+                    bio.setText(bioData);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
