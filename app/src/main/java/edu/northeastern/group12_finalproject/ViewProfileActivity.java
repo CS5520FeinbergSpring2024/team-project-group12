5694c2f2a6549e4e6ef88573baa67c7aa86a4be9
RrrS2022 <sun.yuhu@northeastern.edu>
2024-04-12 11:18:45 -0400

    update/avoid conflict

--- app/src/main/java/edu/northeastern/group12_finalproject/ViewProfileActivity.java
+++ app/src/main/java/edu/northeastern/group12_finalproject/ViewProfileActivity.java
@@ -1,8 +1,11 @@
 package edu.northeastern.group12_finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -11,6 +14,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ViewProfileActivity extends AppCompatActivity {
@@ -23,8 +27,8 @@ public class ViewProfileActivity extends AppCompatActivity {
 
     TextView viewProfileName;
     TextView displayNameTv;
-    TextView bio;
-    TextView editTv;
+    TextView viewProfileBio;
+    TextView tvFollow;
 
     PostAdapter adapter;
 
@@ -34,5 +38,35 @@ public class ViewProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
+
+        // Firebase initialization.
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        RecyclerView recyclerView = findViewById(R.id.profileRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        posts = new ArrayList<>();
+
+        // Progress bar
+        progBar = (ProgressBar) findViewById(R.id.profileProgressBar);
+        progBar.setVisibility(View.GONE);
+
+        // Retrieve user information.
+        retrieveFirebaseInfo();
+
+        // Retrieve user posts.
+        retrievePosts();
+
+
+        // Set up adapter
+        adapter = new PostAdapter(posts);
+        recyclerView.setAdapter(adapter);
+
+        // Set up follow action
+        tvFollow = findViewById(R.id.)
+
     }
 }
