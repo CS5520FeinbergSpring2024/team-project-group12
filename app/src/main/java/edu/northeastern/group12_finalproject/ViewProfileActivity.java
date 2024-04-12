9a4c26b1d3bf0c7544b30b27982d9fedc907bfb8
RrrS2022 <sun.yuhu@northeastern.edu>
2024-04-12 11:10:05 -0400

    update

--- app/src/main/java/edu/northeastern/group12_finalproject/ViewProfileActivity.java
+++ app/src/main/java/edu/northeastern/group12_finalproject/ViewProfileActivity.java
@@ -3,9 +3,33 @@ package edu.northeastern.group12_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
 
 public class ViewProfileActivity extends AppCompatActivity {
 
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference usersDatabaseReference;
+    DatabaseReference postsDatabaseReference;
+
+    TextView viewProfileName;
+    TextView displayNameTv;
+    TextView bio;
+    TextView editTv;
+
+    PostAdapter adapter;
+
+    private ProgressBar progBar;
+    private List<Post> posts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
