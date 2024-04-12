7daaea7374713397f5609111090ef49307450aed
RrrS2022 <sun.yuhu@northeastern.edu>
2024-04-12 12:11:08 -0400

    add parcelable

--- app/src/main/java/edu/northeastern/group12_finalproject/Users.java
+++ app/src/main/java/edu/northeastern/group12_finalproject/Users.java
@@ -1,6 +1,11 @@
 package edu.northeastern.group12_finalproject;
 
-public class Users {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class Users implements Parcelable {
     private String bio;
     private String username;
     private String email;
@@ -21,6 +26,27 @@ public class Users {
         this.followed = followed;
         this.uid = uid;
     }
+
+    protected Users(Parcel in) {
+        bio = in.readString();
+        username = in.readString();
+        email = in.readString();
+        following = in.readInt();
+        followed = in.readInt();
+        uid = in.readString();
+    }
+
+    public static final Creator<Users> CREATOR = new Creator<Users>() {
+        @Override
+        public Users createFromParcel(Parcel in) {
+            return new Users(in);
+        }
+
+        @Override
+        public Users[] newArray(int size) {
+            return new Users[size];
+        }
+    };
 
     // getters and setters
     public String getBio() {
@@ -68,5 +94,20 @@ public class Users {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(bio);
+        dest.writeString(username);
+        dest.writeString(email);
+        dest.writeInt(following);
+        dest.writeInt(followed);
+        dest.writeString(uid);
     }
 }
